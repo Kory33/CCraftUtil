9419a93e71d22ba3326252444059acc420751511
Kory33 <konryoitdv@gmail.com>
2016-10-15 03:34:39 +0530

    improved loops/code in refuelMax

--- 1.7.10/refuelMax.lua
+++ 1.7.10/refuelMax.lua
@@ -1,18 +1,23 @@
+-- returns how much more fuel can be filled
+function getFuelFillAmount()
+    return turtle.getFuelLimit() - turtle.getFuelLevel()
+end
+
 -- consume as much fuel as to make the fuel level full
 -- without any redundant use of fuel item
 function refuelMax()
     initSlot = turtle.getSelectedSlot()
-    maxFuel = turtle.getFuelLimit()
-    slot = 1
-    repeat
-        refuelAmount = maxFuel - turtle.getFuelLevel()
+    for slot = 1, 16 do
+        refuelAmount = getFuelFillAmount()
         if(turtle.refuel(1)) then
-            refuelPerItem = refuelAmount - (maxFuel - turtle.getFuelLevel())
+            refuelPerItem = refuelAmount - getFuelFillAmount()
             refuelAmount = refuelAmount - refuelPerItem
             turtle.refuel(math.min(math.ceil(refuelAmount / refuelPerItem), turtle.getItemCount()))
         end
-        slot = slot + 1
-    until(refuelAmount <= 0 or slot == 17)
+        if(getFuelFillAmount == 0) then
+            break
+        end
+    end
     turtle.select(initSlot)
     return
 end
