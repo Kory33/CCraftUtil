67d3134409aa03ede7fb5a9993280243fafa936e
Kory33 <konryoitdv@gmail.com>
2016-10-16 03:58:55 +0530

    implemented inventory-search for replanting/modified sleep param

--- 1.7.10/farming/harvestRectFarm.lua
+++ 1.7.10/farming/harvestRectFarm.lua
@@ -3,6 +3,10 @@
 -- hence no obstructions(solid materials) should be placed at the same level of the crops
 -- and the farm should have guard at the far and right side.
 
+
+-- try to drop all the items into another inventory
+-- that is placed in front, or if given by sDirUD<"Up"|"Down">,
+-- into that direction
 function dropAllItems(sDirUD)
     local initSlot = turtle.getSelectedSlot()
     for slot = 1, 16 do
@@ -17,12 +21,37 @@ function dropAllItems(sDirUD)
 end
 
 
+-- attempt to place any item from the inventory
+-- into the front, or if specified by sDirUD<"Up"|"Down">, into that direction
+function placeAny(sDirUD)
+    local initSlot = turtle.getSelectedSlot()
+    local placed = false
+    for slot = 1, 16 do
+        turtle.select(slot)
+
+        if sDirUD == "Up" or sDirUD == "Down" then
+            placed = placed or turtle["place" .. sDirUD]()
+        else
+            placed = placed or turtle.place()
+        end
+
+        if placed then
+            break
+        end
+    end
+    turtle.select(initSlot)
+    return placed
+end
+
+
+-- harvest and replant if a block is detected below
+-- TODO: implement block-/growth-check
 function harvestIfDetectedDown()
     if turtle.detectDown() then
         turtle.digDown()
     end
     -- attempt to replant
-    return turtle.placeDown()
+    return turtle.placeAny("Down")
 end
 
 
@@ -60,6 +89,7 @@ function harvestRectFarm()
 
         -- move to the next row
         for j = 1, 2 do
+            -- determine the direction to turn according to the direction of the turtle
             if isFacingFar then
                 turtle.turnLeft()
             else 
@@ -71,6 +101,7 @@ function harvestRectFarm()
             end
         end
 
+        -- invert the flag
         isFacingFar = not isFacingFar
     end
 
@@ -91,5 +122,5 @@ while true do
     turtle.back()
     turtle.down()
     dropAllItems("Down")
-    sleep(600)
+    sleep(2400)
 end
