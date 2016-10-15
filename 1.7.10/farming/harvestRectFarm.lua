e4e9ecd042ca85b4c3ee4365fdcc9b95f6878cfa
Kory33 <konryoitdv@gmail.com>
2016-10-16 03:03:54 +0530

    added harvestRectFarm script

---
+++ 1.7.10/farming/harvestRectFarm.lua
@@ -0,0 +1,99 @@
+-- Starting from the left-near side, attempt to harvest the crops
+-- Judging algorithm is based on the turtle.detect method and turtle.forward,
+-- hence no obstructions(solid materials) should be placed at the same level of the crops
+-- and the farm should have guard at the far and right side.
+
+function dropAllItems(sDirUD)
+    local initSlot = turtle.getSelectedSlot()
+    for slot = 1, 16 do
+        turtle.select(slot)
+        if sDirUD == "Up" or sDirUD == "Down" then
+            turtle["drop" .. sDirUD]()
+        else
+            turtle.drop()
+        end
+    end
+    turtle.select(initSlot)
+end
+
+
+function breakIfDetected(sDirUD)
+    if sDirUD == "Up" or sDirUD == "Down" then
+        if turtle["detect" .. sDirUD]() then
+            return turtle["dig" .. sDirUD]()
+        end
+    else
+        if turtle.detect() then
+            return turtle.dig()
+        end
+    end
+    return false
+end
+
+
+-- The turtle is expected to be at the left-near side of the farm
+-- when this funciton is called
+function harvestRectFarm()
+    -- measure the width(near-far) of the farm 
+    local width = 0
+    repeat
+        breakIfDetected("Down")
+        width = width + 1
+    until not turtle.forward()
+    turtle.turnRight()
+
+    -- measure the length(left-right) of the farm
+    local length = 0
+    repeat
+        breakIfDetected("Down")
+        length = length + 1
+    until not turtle.forward()
+    turtle.turnRight()
+
+    -- start harvesting the rest of the farm
+    local isFacingFar = false
+    if width > 1 then
+        turtle.forward()
+        breakIfDetected("Down")
+    end
+
+    for i = 1, length - 1 do
+        for j = 1, width - 2 do
+            turtle.forward()
+            breakIfDetected("Down")
+        end
+
+        -- move to the next row
+        for j = 1, 2 do
+            if isFacingFar then
+                turtle.turnLeft()
+            else 
+                turtle.turnRight()
+            end
+            if j == 1 then 
+                turtle.forward()
+            end
+        end
+
+        isFacingFar = not isFacingFar
+    end
+
+    -- bring back the turtle to the oroginal place if not already reached
+    if not isFacingFar then
+        for i = 1, width - 2 do
+            turtle.forward()
+        end
+        turtle.turnLeft()
+        turtle.turnLeft()
+    end
+end
+
+while true do
+    turtle.up()
+    turtle.forward()
+    harvestRectFarm()
+    turtle.back()
+    turtle.down()
+    dropAllItems("Down")
+    sleep(600)
+end
