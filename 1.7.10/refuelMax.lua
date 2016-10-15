53bd6aeb3aef04c0de642ddee10661b77019214c
Kory33 <konryoitdv@gmail.com>
2016-10-16 00:04:50 +0530

    modified scope for refuelMax

--- 1.7.10/refuelMax.lua
+++ 1.7.10/refuelMax.lua
@@ -6,13 +6,13 @@ end
 -- consume as much fuel as to make the fuel level full
 -- without any redundant use of fuel item
 function refuelMax()
-    initSlot = turtle.getSelectedSlot()
+    local initSlot = turtle.getSelectedSlot()
     for slot = 1, 16 do
         turtle.select(slot)
-        refuelAmount = getFuelFillAmount()
+        local refuelAmount = getFuelFillAmount()
         if(turtle.refuel(1)) then
-            refuelPerItem = refuelAmount - getFuelFillAmount()
-            refuelAmount = refuelAmount - refuelPerItem
+            local refuelPerItem = refuelAmount - getFuelFillAmount()
+            local refuelAmount = refuelAmount - refuelPerItem
             turtle.refuel(math.min(math.ceil(refuelAmount / refuelPerItem), turtle.getItemCount()))
         end
         if(getFuelFillAmount() == 0) then
